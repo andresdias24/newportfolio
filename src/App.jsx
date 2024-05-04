87afef0bb7976f6f63f7c0d65a2f45958053f763
Harold Diaz <harold.vargas@bewe.io>
2024-05-04 15:25:55 -0500

    test deploy gh pages

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>nuevo portafolio para google</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -26,8 +26,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        vamos a implementar un nuevo formato      </p>
     </>
   )
 }

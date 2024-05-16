4cbb75774cac2c6d4349230152652e29f1e94ad3
Harold Diaz <65746887+andresdias24@users.noreply.github.com>
2024-05-15 19:22:04 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>nuevo portafolio para google</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        vamos a implementar un nuevo formato      </p>
+        <h1> portafolio 3d</h1>
     </>
   )
 }

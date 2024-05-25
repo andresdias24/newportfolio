39465726d1217ff41b87e2fb0a914de75398cc9d
Harold Diaz <harold.vargas@bewe.io>
2024-05-25 16:11:57 -0500

    fix

--- src/constants/index.js
+++ src/constants/index.js
@@ -171,35 +171,35 @@ const experiences = [
 ];
 
 const testimonials = [
-    {
-        testimonial:
-            "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
-        name: "Sara Lee",
-        designation: "CFO",
-        company: "Acme Co",
-        image: "https://randomuser.me/api/portraits/women/4.jpg",
-    },
-    {
-        testimonial:
-            "I've never met a web developer who truly cares about their clients' success like Rick does.",
-        name: "Chris Brown",
-        designation: "COO",
-        company: "DEF Corp",
-        image: "https://randomuser.me/api/portraits/men/5.jpg",
-    },
-    {
-        testimonial:
-            "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
-        name: "Lisa Wang",
-        designation: "CTO",
-        company: "456 Enterprises",
-        image: "https://randomuser.me/api/portraits/women/6.jpg",
-    },
+//     {
+//         testimonial:
+//             "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+//         name: "Sara Lee",
+//         designation: "CFO",
+//         company: "Acme Co",
+//         image: "https://randomuser.me/api/portraits/women/4.jpg",
+//     },
+//     {
+//         testimonial:
+//             "I've never met a web developer who truly cares about their clients' success like Rick does.",
+//         name: "Chris Brown",
+//         designation: "COO",
+//         company: "DEF Corp",
+//         image: "https://randomuser.me/api/portraits/men/5.jpg",
+//     },
+//     {
+//         testimonial:
+//             "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+//         name: "Lisa Wang",
+//         designation: "CTO",
+//         company: "456 Enterprises",
+//         image: "https://randomuser.me/api/portraits/women/6.jpg",
+//     },
 ];
 
 const projects = [
     {
-        name: "Car Rent",
+        name: "E-commerce",
         description:
             "Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
         tags: [
@@ -211,16 +211,12 @@ const projects = [
                 name: "mongodb",
                 color: "green-text-gradient",
             },
-            {
-                name: "tailwind",
-                color: "pink-text-gradient",
-            },
         ],
         image: carrent,
         source_code_link: "https://github.com/",
     },
     {
-        name: "Job IT",
+        name: "REST_API",
         description:
             "Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
         tags: [
